729bf9c309b465f7fc5550d190bf8caa52783688
Mailza <mailza.souzalima2023@gmail.com>
2024-07-29 13:28:15 -0300

    acrescentado chave estrangeira

--- src/main/java/com/aulanelio/usuariodepto/entities/User.java
+++ src/main/java/com/aulanelio/usuariodepto/entities/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -20,6 +21,7 @@ public class User {
 	private String email;
 	
 	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {

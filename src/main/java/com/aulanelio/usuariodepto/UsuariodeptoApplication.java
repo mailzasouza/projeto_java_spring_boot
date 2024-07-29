7cd8f7a8006f18711751e755a605bdba94e0f1aa
Mailza <mailza.souzalima2023@gmail.com>
2024-07-29 13:16:12 -0300

    mapeamento objeto relacional das entidades

---
+++ src/main/java/com/aulanelio/usuariodepto/UsuariodeptoApplication.java
@@ -0,0 +1,13 @@
+package com.aulanelio.usuariodepto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariodeptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariodeptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulanelio/usuariodepto/UsuariodeptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulanelio.usuariodepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariodeptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
